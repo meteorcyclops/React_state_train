4126718b64499b304cc8a3b6e89af8cdbee51bbc
meteorcyclops1104 <meteorcyclops1104@gmail.com>
2016-07-28 17:23:48 +0800

    update state_train.js

--- src/state_train.js
+++ src/state_train.js
@@ -8,10 +8,7 @@ import Radium from 'radium'
 class Login extends Component {
 
 	componentDidMount() {
-		$('.message a').click(function (e) {
-			$('form').animate({ height: "toggle", opacity: "toggle" }, "slow")
-			e.preventDefault()
-		})
+
 	}
 
 	componentWillMount() {
@@ -28,19 +25,6 @@ class Login extends Component {
 		return (
 			<div className="login-page">
 				<div className="form" style={styles.base}>
-					<form className="register-form">
-						<input type="text" placeholder="name"/>
-						<input type="password" placeholder="password"/>
-						<input type="text" placeholder="email address"/>
-						<button>create</button>
-						<p className="message">Already registered?<a href="#">Sign In</a></p>
-					</form>
-					<form className="login-form">
-						<input type="text" placeholder="username"/>
-						<input type="password" placeholder="password"/>
-						<button>login</button>
-						<p className="message">Not registered?<a href="#">Create an account</a></p>
-					</form>
 				</div>
 			</div>
 		)
