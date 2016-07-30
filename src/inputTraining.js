41139cec3b3194bdfd5d49f768c1c1dd4ee93bb6
meteorcyclops1104 <meteorcyclops1104@gmail.com>
2016-07-30 16:03:53 +0800

    input demo init. add new reatTemplate in IDE.

---
+++ src/inputTraining.js
@@ -0,0 +1,31 @@
+/**
+ * Created by meteorcyclops1104 on 2016/7/29.
+ */
+
+import React, {Component} from 'react'
+import $ from 'jquery'
+import Radium from 'radium'
+
+@Radium
+class InputProject extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillMount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default InputProject
--- src/test.js
+++ src/test.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react'
 import stateComponent from './state_train';
 import NewObject from './stateTraining'
+import InputProject from './inputTraining'
 
 class Test extends Component {
     render() {
         return (
             <div>
                 <h1 style={{textAlign: "center"}}>
-                    <NewObject/>
+                    <InputProject/>
                 </h1>
             </div>
         )
